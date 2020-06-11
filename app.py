0d39b240c480e6c047d67eb0269959582fbbe211
Daniel Thompson <DanThomp507@gmail.com>
2020-06-11 10:04:32 -0400

    updated app and config files

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "DDOS Protector API"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
